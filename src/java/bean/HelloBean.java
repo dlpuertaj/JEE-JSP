931b36540504c9b687bc970f07a456ae69cf557a
David Leonardo <dlpuertaj@unal.edu.co>
2018-03-16 13:21:44 -0500

    Ejemplo de Servlets Libro:

---
+++ src/java/bean/HelloBean.java
@@ -0,0 +1,28 @@
+package bean;
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author dlpuertaj
+ */
+public class HelloBean {
+    String name;
+    
+    public HelloBean(){}
+    
+    public String getName(){return name;}
+    public void setName(String name){this.name = name;}
+    
+    public String getGello(){
+        if(name == null)
+            return "Hello every body";
+        else
+            return "Hello" + name;
+    }
+}
